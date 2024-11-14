1a0b624cffca49e911d3d7f36a7fae09749884a2
yoonseoc126 <yoonseoc@usc.edu>
2024-11-13 23:31:45 -0800

    Initial commit and login

---
+++ Tonic/TonicApp.swift
@@ -0,0 +1,17 @@
+//
+//  TonicApp.swift
+//  Tonic
+//
+//  Created by Yoonseo Choi on 11/13/24.
+//
+
+import SwiftUI
+
+@main
+struct TonicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TonicTests/TonicTests.swift
@@ -0,0 +1,17 @@
+//
+//  TonicTests.swift
+//  TonicTests
+//
+//  Created by Yoonseo Choi on 11/13/24.
+//
+
+import Testing
+@testable import Tonic
+
+struct TonicTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
