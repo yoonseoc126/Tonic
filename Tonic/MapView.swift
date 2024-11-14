3a95aa4b85ca90d596bee974eaf25bbd2e42d011
yoonseoc126 <yoonseoc@usc.edu>
2024-11-14 00:17:14 -0800

    Add basic map

---
+++ Tonic/MapView.swift
@@ -0,0 +1,16 @@
+//
+//  MapView.swift
+//  Tonic
+//
+//  Created by Yoonseo Choi on 11/13/24.
+//
+
+import MapKit
+import SwiftUI
+
+struct MapView: View {
+    
+    var body: some View {
+        Map()
+    }
+}
